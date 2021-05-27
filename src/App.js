1b14e59d7dd932cecb1bf42464447b35bf386d75
Anas <anasarif216@gmail.com>
2021-05-27 20:50:18 +0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+
+import React, {Component} from 'react'
+import Contact from './components/Contact'
+
+// import './App.css';
+
+class App extends Component {
+  render(){
+
+    return(
+      <div className='App'>
+        <h1>The App Component</h1>
+        <Contact />
+        <Contact />
+        <Contact />
+
+      </div>
+    )
+  }
+}
+
+export default App
