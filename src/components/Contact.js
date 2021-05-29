4bab55d622ff8ec23dbe866a388c33fbf0f21611
Anas <anasarif216@gmail.com>
2021-05-29 23:31:14 +0500

    adding icons

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,16 +2,33 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types';
 
 class Contact extends Component {
+  state = {
+    showContactInfo: false
+  }
+
+
   render() {
     const { name, email, phone } = this.props.contact;
+    const { showContactInfo } = this.state
+
 
     return (
       <div className='card card-body mb-3'>
-        <h4>{name}</h4>
+        <h4  >{name} 
+        <i onClick={() => 
+          this.setState({showContactInfo: !this.state.showContactInfo})}
+          className='drop-down fas fa-sort-down' style={{fontSize:'24px'}}/>
+
+          <i className='cross fas fa-times' style={{cursor:'pointer', float: 'right', color:'red'}} />
+          </h4>
+        
+        {showContactInfo ? (        
         <ul className='list-group'>
           <li className='list-group-item'>Email: {email}</li>
           <li className='list-group-item'>Phone: {phone}</li>
-        </ul>    
+        </ul>) : null}
+
+            
       </div>
     );
   }
