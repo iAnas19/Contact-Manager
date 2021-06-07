0ee92e8126eaae801ab447be9f3510e954e4cef6
Muhammad Anas <anasarif216@gmail.com>
2021-06-08 02:40:18 +0500

    adding context reducer

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,41 +1,50 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types';
+import { Consumer } from '../context';
 
 class Contact extends Component {
   state = {
     showContactInfo: false
   }
 
-
-  onDeleteClick = () => {
-    this.props.deleteClickHandler()
+  onDeleteClick = (id, dispatch) => {
+    dispatch({type: 'DELETE_CONTACT', payload: id});    
+     
   }
 
   render() {
-    const { name, email, phone } = this.props.contact;
+    const { id, name, email, phone } = this.props.contact;
     const { showContactInfo } = this.state
 
-
     return (
-      <div className='card card-body mb-3'>
-        <h4>
-          {name} <i onClick={() => 
-          this.setState({showContactInfo: !this.state.showContactInfo})}
-          className='drop-down fas fa-sort-down' style={{fontSize:'24px'}}/>
-
-          <i className='cross fas fa-times' style={{cursor:'pointer', float: 'right', color:'red'}}
-          onClick={this.onDeleteClick } 
-          />
-        </h4>
-        
-        {showContactInfo ? (        
-        <ul className='list-group'>
-          <li className='list-group-item'>Email: {email}</li>
-          <li className='list-group-item'>Phone: {phone}</li>
-        </ul>) : null}
-
+      <Consumer>
+        {value => {
+          const {dispatch } = value
+          return(
+            <div className='card card-body mb-3'>
+            <h4>
+              {name} <i onClick={() => 
+              this.setState({showContactInfo: !this.state.showContactInfo})}
+              className='drop-down fas fa-sort-down' style={{fontSize:'24px'}}/>
+    
+              <i className='cross fas fa-times' style={{cursor:'pointer', float: 'right', color:'red'}}
+              onClick={this.onDeleteClick.bind(this, id, dispatch) } 
+              />
+            </h4>
             
-      </div>
+            {showContactInfo ? (        
+            <ul className='list-group'>
+              <li className='list-group-item'>Email: {email}</li>
+              <li className='list-group-item'>Phone: {phone}</li>
+            </ul>) : null}
+    
+                
+          </div>
+
+          )
+        }}
+      </Consumer>
+
     );
   }
 }
@@ -49,7 +58,6 @@ Contact.defaultProps = {
 
 Contact.propTypes = {
   contact:PropTypes.object.isRequired,
-  deleteClickHandler: PropTypes.func.isRequired
     
   }
 
